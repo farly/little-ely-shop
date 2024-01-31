694b80b6150061d966b17fd8ffb7058219dddf9c
Vincent Taboada <vftaboada@gmail.com>
2024-01-31 20:51:11 +0800

    intial setup little ely shop

---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { useParams } from "next/navigation"
+
+const ProductPage = () => {
+	const { id } = useParams()
+	
+	return (
+		<div>{id}</div>
+	)
+}
+
+export default ProductPage
